c259b5ec1cb77159de7f65ad5cde86ff0ed452b4
Nabin Garai <92241678+nabing259@users.noreply.github.com>
2023-07-21 12:06:47 +0530

    Update report.js

--- report.js
+++ report.js
@@ -1,7 +1,4 @@
-// write code here, dont change anything in HTML and css files 
-// refer to id's mentioned in html files carefully
-// Follow the exact column order for tables,for eg: name should be filled under "name" column 
-// we are using form here, make sure you are using "submit" eventListener , or else you will get 0 marks
+
 
 document.querySelector("form").addEventListener("submit", funReport);
 
